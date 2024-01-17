de423f69bd5a91a639ea17970c66c164e85dc9b7
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-01-17 16:18:43 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key  = "AKIAQ3EGT6YYWUZKHZPF"
-  secret_key = "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"
+  access_key  = "AKIAQ3EGT6YYW"
+  secret_key = "boqKlBBwXtxdStd"
 }
 
 #create a security group for RDS Database Instance
